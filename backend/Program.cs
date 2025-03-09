95dc82f75b50481a8934d61dbe8f94c2ce973ac5
azooz59T <mohamedoo1997@hotmail.com>
2025-03-09 14:03:26 +0000

    Add dependency injection for the AutoMapper
    + Add Json converter to convert Enums to Strings

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,7 @@
+using backend.Core.AutoMapperConfig;
 using backend.Core.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,14 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("local"));
 });
 
-builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(AutoMapperConfigProfile));
+
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
